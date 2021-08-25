f8bda6c60e5f8c05d9ad31e938e89d76540b7784
Yudi Edri Alviska <yudi.a@weefer.co.id>
2021-08-25 14:53:46 +0700

    remove @adrianha/react-native-midtrans

--- App.js
+++ App.js
@@ -1,34 +1,7 @@
 import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
-import Midtrans from '@adrianha/react-native-midtrans';
-
-const midtrans = new Midtrans({
-  clientKey: 'CLIENT_KEY',
-  baseUrl: 'BASE_URL',
-
-  /** iOS only: Midtrans.ENVIRONMENT_SANDBOX | Midtrans.ENVIRONMENT_PRODUCTION */
-  environment: Midtrans.ENVIRONMENT_SANDBOX,
-
-  /** android only */
-  colorTheme: {
-    primaryColor: '#000000',
-  },
-});
 
 export default function App(props) {
-
-  useEffect(() => {
-    payment()
-  }, [])
-
-  async function payment() {
-    try {
-      const result = await midtrans.startPaymentWithSnapToken('SNAP_TOKEN');
-      console.log({ result });
-    } catch (e) {
-      console.log(e);
-    }
-  }
 
   return (
     <View style={{
